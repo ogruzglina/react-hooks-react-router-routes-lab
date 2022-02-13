a85f01b6acd4d435dd3cb9dac34651abd31a4d15
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-12 19:15:49 -0500

    commit tree e976ced287d1bf18bcdd42693948d88f8f9a1289

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const showDirectors = directors.map( director => (
+    <div key = { director.name }>
+      <h3>Name: { director.name }</h3>
+      Movies:
+        <ul>
+          { director.movies.map( movie => <li key = { movie }>{ movie }</li>)}
+        </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      { showDirectors }
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const showMovies = movies.map( movie => (
+    <div key = {movie.title}>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: { movie.time }</p>
+      Genres:
+        <ul>
+          { movie.genres.map( genre => <li key = { genre }>{ genre }</li>) }
+        </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      { showMovies }
+    </div>
+  );
 }
 
 export default Movies;
